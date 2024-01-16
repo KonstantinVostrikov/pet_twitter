7321d64c183a8c0a02a072924f73654b0b1d899e
Konstatnin Vostrikov <konstantin.vostrikov@gridnine.com>
2024-01-16 13:20:34 +0600

    cover UserService with tests

--- src/test/groovy/com/vostrikov/pet_twitter/services/impl/UserServiceImplSpec.groovy
+++ src/test/groovy/com/vostrikov/pet_twitter/services/impl/UserServiceImplSpec.groovy
@@ -1,5 +1,6 @@
 package com.vostrikov.pet_twitter.services.impl
 
+import com.vostrikov.pet_twitter.dto.FavoritePost
 import com.vostrikov.pet_twitter.dto.Subscription
 import com.vostrikov.pet_twitter.dto.User
 import com.vostrikov.pet_twitter.exceptions.subscription.SubscriptionException
@@ -195,6 +196,8 @@ class UserServiceImplSpec extends Specification {
         thrown(UserWithNicknameAlreadyExistException)
     }
 
+
+    // subscribe
     def "subscribe should add subscription if DTO is correct and users exists"() {
         given:
         def userFrom = new User(id: "userFromId", email: "from@example.com", nickName: "fromUser", subscriptions: new HashSet<String>())
@@ -255,5 +258,117 @@ class UserServiceImplSpec extends Specification {
 
         then:
         thrown(SubscriptionException)
+    }
+
+
+    // unsubscribe
+    def "unsubscribe should remove subscription if DTO is correct and users exist"() {
+        given:
+        def userFrom = new User(id: "userFromId", email: "from@example.com", nickName: "fromUser", subscriptions: ["userToId"])
+        def userTo = new User(id: "userToId", email: "to@example.com", nickName: "toUser")
+        def subscription = new Subscription(from: "userFromId", to: "userToId")
+
+        userRepository.findById("userFromId") >> Optional.of(userFrom)
+        userRepository.findById("userToId") >> Optional.of(userTo)
+        userRepository.save(_) >> { userFrom.subscriptions.remove("userToId") }
+
+        when:
+        def result = userService.unsubscribe(subscription)
+
+        then:
+        1 * userRepository.findById("userFromId") >> Optional.of(userFrom)
+        1 * userRepository.findById("userToId") >> Optional.of(userTo)
+        1 * userRepository.save(userFrom) >> userFrom
+
+        result == true
+        !userFrom.subscriptions.contains("userToId")
+    }
+
+    def "unsubscribe should throw SubscriptionException if 'from' is empty"() {
+        when:
+        userService.unsubscribe(new Subscription(from: "", to: "userToId"))
+
+        then:
+        thrown(SubscriptionException)
+    }
+
+    def "unsubscribe should throw SubscriptionException if 'to' is empty"() {
+        when:
+        userService.unsubscribe(new Subscription(from: "userFromId", to: ""))
+
+        then:
+        thrown(SubscriptionException)
+    }
+
+    def "unsubscribe should throw SubscriptionException if user 'from' does not exist"() {
+        given:
+        userRepository.findById("nonExistingUserId") >> Optional.empty()
+        userRepository.findById("userToId") >> Optional.of(new User())
+
+        when:
+        userService.unsubscribe(new Subscription(from: "nonExistingUserId", to: "userToId"))
+
+        then:
+        thrown(SubscriptionException)
+    }
+
+    def "unsubscribe should throw SubscriptionException if user 'to' does not exist"() {
+        given:
+        userRepository.findById("userFromId") >> Optional.of(new User())
+        userRepository.findById("nonExistingUserId") >> Optional.empty()
+
+        when:
+        userService.unsubscribe(new Subscription(from: "userFromId", to: "nonExistingUserId"))
+
+        then:
+        thrown(SubscriptionException)
+    }
+
+    // favoritePost
+    def "favoritePost should add post to favorites if it doesn't exist and user exists"() {
+        given:
+        def user = new User(id: "userId", email: "test@example.com", nickName: "testUser", favoritePosts: [])
+        def favoritePost = new FavoritePost(userId: "userId", postId: "postId")
+
+        userRepository.findById("userId") >> Optional.of(user)
+        userRepository.save(_) >> { User u -> u }
+
+        when:
+        userService.favoritePost(favoritePost)
+
+        then:
+        1 * userRepository.findById("userId") >> Optional.of(user)
+        1 * userRepository.save(user) >> _
+
+        user.favoritePosts.contains("postId")
+    }
+
+    def "favoritePost should remove post from favorites if it exists and user exists"() {
+        given:
+        def user = new User(id: "userId", email: "test@example.com", nickName: "testUser", favoritePosts: ["postId"])
+        def favoritePost = new FavoritePost(userId: "userId", postId: "postId")
+
+        userRepository.findById("userId") >> Optional.of(user)
+        userRepository.save(_) >> { User u -> u }
+
+        when:
+        userService.favoritePost(favoritePost)
+
+        then:
+        1 * userRepository.findById("userId") >> Optional.of(user)
+        1 * userRepository.save(user) >> _
+
+        !user.favoritePosts.contains("postId")
+    }
+
+    def "favoritePost should throw RuntimeException if user doesn't exist"() {
+        given:
+        userRepository.findById("nonExistingUserId") >> Optional.empty()
+
+        when:
+        userService.favoritePost(new FavoritePost(userId: "nonExistingUserId", postId: "postId"))
+
+        then:
+        thrown(RuntimeException)
     }
 }
